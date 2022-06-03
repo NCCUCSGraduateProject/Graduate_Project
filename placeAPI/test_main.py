5ea3c56a693a3b244c18f1190062d75e8b6f543f
24135768b <24135768b@gmail.com>
2022-06-03 16:20:18 +0800

    add comments

--- placeAPI/test_main.py
+++ placeAPI/test_main.py
@@ -1,10 +1,23 @@
+# example of using getLocation()
+
 import placesQuery
 import os
 import json
 
 radius = 250
+
+# function getlocation: 
+# parameters: 
+#   lat: int, no default
+#   lon: int, no default
+#   radius: int, default = "200"
+#
+# returns a tuple (a,b) 
+#   a is a list of restaurant names
+#   b is a list of informations about each restautant in Json format
 restaurantNames, resultList = placesQuery.getLocation(24.9870522,121.575362,radius)
 
+# save the results into files
 with open(os.path.join('results/name',str(radius)+'.txt'), 'w') as f:
     for item in restaurantNames:
         f.write("%s\n" % item)
