7dcdfa56691c33ee5ea45203d1e7b28cedfb1d8d
YJack0000 <jackleagends@gmail.com>
2022-08-18 15:32:51 +0800

    試檔案

---
+++ backend/test.js
@@ -0,0 +1,13 @@
+const axios = require('axios');
+
+axios.get("http://localhost:8080/direction/nearbyPoints", {
+    params: {
+        originLat: 24.9885267,
+        originLng: 121.5739302,
+        destLat: 25.0342531,
+        destLng: 121.5270531,
+        limitDistance: 1000
+    }
+}).then(function(response){
+    console.log(response.data[0])
+})
