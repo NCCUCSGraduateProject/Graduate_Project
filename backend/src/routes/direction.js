aeb96223c5a2b4731feea1598a79145305e09a79
Mark Lai <laimark900317@gmail.com>
2022-10-27 02:57:49 +0800

    fix: split range default 1000

--- backend/src/routes/direction.js
+++ backend/src/routes/direction.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const direction = require('../services/direction.js')
 
 router.get('/nearbyPoints', async (req, res) => {
-  let result  = await direction.nearbyPoints(req.query.originLat, req.query.originLng, req.query.destLat, req.query.destLng, req.query.limitDistance, req.query.splitRange)
+  let result  = await direction.nearbyPoints(req.query.originLat, req.query.originLng, req.query.destLat, req.query.destLng, req.query.limitDistance, req.query.splitRange || 1000)
  // res.json({"hello":"hello"})
   res.json(result)
   
