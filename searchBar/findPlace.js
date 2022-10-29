7160caf1b4fcf3844d7de92a8a157ad8a0eedd26
tsai <24135768b@gmail.com>
2022-10-30 04:53:42 +0800

    .

--- searchBar/findPlace.js
+++ searchBar/findPlace.js
@@ -1,4 +1,8 @@
-var MongoClient = require('mongodb').MongoClient;
+const MongoClient = require('mongodb').MongoClient;
+const axios = require('axios');
+const API_KEY = require('./constant.js');
+
+
 
 // Connect to the db
 const localUrl  = 'mongodb://localhost:27017';
@@ -15,8 +19,17 @@ MongoClient.connect(remoteUrl, async function(err, client){
     
     const document = await db.collection('map').findOne(query, options);
     console.log(document);
+
     if(!document) {
-      console.log('No document found');
+
+      console.log('No document found, adding new document');
+      const placeDetails = await getPlaceDetails(query.place_id);
+      const newDocument = modifyDocument(placeDetails);
+      const result = await db.collection('map').insertOne(newDocument);
+      console.log(result);
+
+    } else {
+      console.log('Document found');
     }
 
   } catch (err) {
@@ -26,3 +39,29 @@ MongoClient.connect(remoteUrl, async function(err, client){
   }
 
 });
+
+const getPlaceDetails = async function(place_id) {
+
+  var config = {
+    method: 'get',
+    url: 'https://maps.googleapis.com/maps/api/place/details/json?place_id=' + place_id + '&fields=name%2Crating%2Cformatted_phone_number&key=' + API_KEY,
+    headers: { }
+  };
+
+  let response = await axios(config);
+  console.log(response.data)
+  return response.data;
+
+}
+
+
+const modifyDocument = function(document) {
+
+  geometry = {
+    type: 'Point',
+    coordinates: [document.geometry.location.lng, document.geometry.location.lat]
+  }
+  document.geometry = geometry;
+
+  return document;
+}
