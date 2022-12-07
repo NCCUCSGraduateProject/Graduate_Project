01e9c2f481e84b279a87eb4a12afb42aab652c91
24135768b <24135768b@gmail.com>
2022-12-07 15:14:07 +0800

    test

--- word2vec/db.py
+++ word2vec/db.py
@@ -1,12 +1,21 @@
 import pymongo
-myclient = pymongo.MongoClient("mongodb+srv://mark:WNQmnmMW1Eob4gFi@cluster0.gvyaavk.mongodb.net/?retryWrites=true&w=majority")
+
+localUrl = 'mongodb://localhost:27017'
+oldRemoteUrl = 'mongodb+srv://mark:WNQmnmMW1Eob4gFi@cluster0.gvyaavk.mongodb.net/?retryWrites=true&w=majority'
+newRemoteUrl = 'mongodb://localhost:57017'
+
+myclient = pymongo.MongoClient(newRemoteUrl)
 mydb = myclient["gp"]
 mycol = mydb["map"]
 
 print('connect mongo\n')
 
+count = 0
+i = 0
+
 for doc in mycol.find():
-    if "tourist_attraction" in doc["types"] or "amusement_park" in doc["types"]:
-        print(doc['reviews'])
-    
+    print(doc)
+    # if "tourist_attraction" in doc["types"] or "amusement_park" in doc["types"]:
+        # count += 1
+
 myclient.close()
