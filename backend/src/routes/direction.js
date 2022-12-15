da0e389ffabd5bede9e759879a3ba91e74abd23f
YJack0000 <jackleagends@gmail.com>
2022-12-15 14:02:17 +0800

    add string

--- backend/src/routes/direction.js
+++ backend/src/routes/direction.js
@@ -4,7 +4,7 @@ const direction = require('../services/direction.js')
 
 router.post('/nearbyPoints', async (req, res) => {
   console.log(req.body.originLat, req.body.originLng, req.body.destLat, req.body.destLng, req.body.limitDistance, req.body.splitRange || 1000, req.body.directionMode || 'driving')
-  let result  = await direction.nearbyPoints(req.body.originLat, req.body.originLng, req.body.destLat, req.body.destLng, req.body.limitDistance, req.body.splitRange || 1000, req.body.directionMode || 'driving', req.body.queryVectors)
+  let result  = await direction.nearbyPoints(req.body.originLat, req.body.originLng, req.body.destLat, req.body.destLng, req.body.limitDistance, req.body.splitRange || 1000, req.body.directionMode || 'driving', req.body.queryString, req.body.queryVectors)
   res.json(result)
   
 })
