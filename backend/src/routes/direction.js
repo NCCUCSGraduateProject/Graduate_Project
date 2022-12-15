33ec9360e284a5549d0c50e8156a5e49ec42ca57
YJack0000 <jackleagends@gmail.com>
2022-12-15 13:10:46 +0800

    print query

--- backend/src/routes/direction.js
+++ backend/src/routes/direction.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const direction = require('../services/direction.js')
 
 router.post('/nearbyPoints', async (req, res) => {
+  console.log(req.body.originLat, req.body.originLng, req.body.destLat, req.body.destLng, req.body.limitDistance, req.body.splitRange || 1000, req.body.directionMode || 'driving')
   let result  = await direction.nearbyPoints(req.body.originLat, req.body.originLng, req.body.destLat, req.body.destLng, req.body.limitDistance, req.body.splitRange || 1000, req.body.directionMode || 'driving', req.body.queryVectors)
   res.json(result)
   
