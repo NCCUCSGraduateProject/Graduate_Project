a0e90702ef5ed3220734205432ee2bf32f629b0d
YJack0000 <jackleagends@gmail.com>
2022-12-15 13:01:18 +0800

    cors python

--- word2vec/api.py
+++ word2vec/api.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+from flask_cors import CORS
 from gevent import pywsgi
 import jieba
 import spacy
@@ -43,6 +44,7 @@ def word2vec(words):
 
 
 app = Flask(__name__)
+CORS(app)
 @app.route('/test', methods=['get'])
 def index():
     
