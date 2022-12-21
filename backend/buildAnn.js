c24e9ee65691cb76a9f4def112d2e9017e72dc1e
24135768b <24135768b@gmail.com>
2022-12-21 23:56:07 +0800

    feat: build Ann trees

---
+++ backend/buildAnn.js
@@ -0,0 +1,42 @@
+const MongoClient = require('mongodb').MongoClient;
+const Annoy = require('annoy');
+const fs = require('fs');
+
+
+// read place_if from placeIDs.txt
+fs.readFile('placeIDs.txt', 'utf8', async (err, data) => {
+    if (err) {  
+        console.error(err);
+        return;
+    }
+    const placeIDs = data.split('\n');
+
+    const client = await MongoClient.connect("mongodb://localhost:27017")
+    console.log("Connected successfully to mongo server")
+    const db = client.db("gp")
+    const collection = db.collection("map")
+
+    for(let i = 0; i < placeIDs.length; i++) {
+        const document = await collection.findOne({place_id: 'ChIJPe6kAKONbzQR4P4I1CExJFs'})
+        console.log(document)
+
+        let tree = new Annoy(300, 'angular')
+        
+        if(document['reviews_spacy'] !== undefined){
+            let reviews_spacy = document.reviews_spacy
+            for(let j = 0; j < reviews_spacy.length; j++) {
+                tree.addItem(j, reviews_spacy[j])
+            }
+            tree.build()
+
+            let path = './AnnTrees/' + placeIDs[i] + '.ann'
+            tree.save(path)
+        }
+        if(i % 100 === 0) 
+            console.log(i)
+        
+        
+    }
+    
+});
+
